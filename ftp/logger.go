6b5ed7b1929b79e9881cdd2fe12fd6d3603d9864
Lars <lars.scheme@gmail.com>
2014-11-26 14:48:41 +0100

    Added comment for the included logger.

--- ftp/logger.go
+++ ftp/logger.go
@@ -2,6 +2,12 @@ package ftp
 
 import "fmt"
 
+// NewConsoleLogger creates a new logger that writes all messages sent and
+// received to the console using fmt.Print. It can be used with ConnectLogging
+// which might be helpful during debugging.
+// If this is used in a GUI, note that ALL messages on the control connection
+// are logged, including the password for the PASS command. You might not want
+// the clear-text password to appear in the GUI.
 func NewConsoleLogger() Logger {
 	return consoleLogger{}
 }
